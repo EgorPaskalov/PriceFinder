db3635a0558e4b7911b638da4c95a339e38c0ff9
Egor Paskalov <paskalovegor84@gmail.com>
2023-10-08 09:50:15 +0300

    Added Wildberries

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 city_link_search_xpath = '//*[@id="__next"]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/form/div/div/label/input'
 
@@ -13,15 +14,18 @@ class Site:
         self.price_xpath = price_xpath
         
 websites = {
-    "Citilink": Site('https://www.citilink.ru',
-    '//*[@id="__next"]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/form/div/div/label/input',
-    '//*[@id="__next"]/div/main/div[1]/div/div[2]/section/div[2]/div[2]/div[1]/div/div/div[3]/div[3]/button/span/div[2]/span/span/span[1]'),
-    "М.Видео": "https://www.mvideo.ru",
-    "Юлмарт": "https://www.ulmart.ru",
-    "Озон": "https://www.ozon.ru"
+    # "Citilink": Site('https://www.citilink.ru',
+    # '//*[@id="__next"]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/form/div/div/label/input',
+    # '//*[@id="__next"]/div/main/div[1]/div/div[2]/section/div[2]/div[2]/div[1]/div/div/div[3]/div[3]/button/span/div[2]/span/span/span[1]'),
+    # "М.Видео": Site("https://www.mvideo.ru",
+    # '//*[@id="1"]',
+    # '/html/body/div[2]/div/div[3]/div/div[5]/div/div[2]/div/div[1]/div/ul/li[1]/div/div[5]/div[1]/div/span[1]'),
+    "Wildberries": Site('https://www.wildberries.ru/',
+    '//*[@id="searchInput"]',
+    '//*[@id="c178630990"]/div/div[3]/p/span/ins')
     }
 
-wish = 'Iphone 15 Pro 128GB' #input('Что вас интересует? Напишите конкретное название товара, и если нужно, его описание')
+wish = 'Iphone 15 Pro 128GB' #input('Что вас интересует? Напишите название товара и его конкретную характеристику: ')
 
 info = {}
 
@@ -31,25 +35,16 @@ wait = WebDriverWait(driver, 10)
 
 def find_first_item_price(url:str, search_path:str , price_path:str):
     driver.get(url)
+    time.sleep(2)
     search = wait.until(EC.presence_of_element_located((By.XPATH, search_path)))
     search.send_keys(wish)
     search.send_keys(Keys.ENTER)
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(7)
     price = driver.find_element(By.XPATH, price_path).text
     return price
 
 for name, site in websites.items():
-    find_first_item_price(site.url, site.search_xpath, site.price_xpath)
-    
-    # elif site == "Юлмарт":
-        # search = wait.until(EC.presence_of_element_located((By.XPATH, '')))
-        # search.send_keys(wish)
-        # search.send_keys(Keys.ENTER)
-    
-    # elif site == "Озон":
-    #     price_element = driver.find_element_by_xpath("//span[@class='c2h5']")
-    #     price = price_element.text
+    price = find_first_item_price(site.url, site.search_xpath, site.price_xpath)
+    info[name] = price
     driver.quit
-print(info, "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
-
-
+print(info, "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
