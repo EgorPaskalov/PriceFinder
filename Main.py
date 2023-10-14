75d0816cd163cc428bdf88e9e1cd5d66cd138d23
Egor Paskalov <paskalovegor84@gmail.com>
2023-10-14 12:36:38 +0300

    Added more Options, fixed Xpath problem, now script can sort price

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 import time
 
 class Site:
@@ -16,28 +17,40 @@ class Site:
 websites = {
     "Citilink": Site('https://www.citilink.ru',
     '//*[@id="__next"]/div/div[3]/div/div[2]/div/div/div[2]/div[1]/form/div/div/label/input',
-    '//*[@id="__next"]/div/main/div[1]/div/div[2]/section/div[2]/div[2]/div[1]/div/div/div[2]/div[3]/a',
+    '//*[@id="__next"]/div/main/div[1]/div/div[2]/section/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div[1]/a',
     '//*[@id="__next"]/div/main/div[1]/div[2]/div/div[4]/div/div[3]/div/div[2]/div/div[1]/span/span/span[1]'),
+
     "М.Видео": Site("https://www.mvideo.ru",
     '//*[@id="1"]', 
-    '/html/body/mvid-root/div/mvid-primary-layout/mvid-layout/div/main/mvid-srp/mvid-product-list-block/div[2]/mvid-product-list/mvid-plp-product-cards-layout/div/mvid-product-cards-row/div[1]/div[2]/mvid-plp-product-title/div/a',  
-    '/html/body/mvid-root/div/mvid-primary-layout/mvid-layout/div/main/mvid-pdp/mvid-pdp-general/div/mvid-general-details/section/div[2]/div/div[2]/mvideoru-product-details-card[1]/div/mvid-price/div/span'),
+    '/html/body/mvid-root/div/mvid-primary-layout/mvid-layout/div/main/mvid-srp/mvid-product-list-block/div[2]/mvid-product-list/mvid-plp-product-cards-layout/div/mvid-product-cards-row/div[1]/div[2]/mvid-plp-product-title/div/a',
+    '/html/body/mvid-root/div/mvid-primary-layout/mvid-layout/div/main/mvid-pdp/mvid-pdp-general/div/mvid-general-details/section/div[2]/div/div[2]/mvideoru-product-details-card[1]/div/mvid-price/div/span[1]'),
+    
     # "Wildberries": Site('https://www.wildberries.ru/',
     # '//*[@id="searchInput"]',
-
-    # '//*[@id="c178632454"]/div/div[3]/p/span/ins')
+    # '//*[@id="c178630990"]/div/a',
+    # '//*[@id="3aaaff51-d9da-e833-4ac9-014928c67d63"]/div[3]/div[2]/div[1]/div/div/div/p[1]/span/ins')
     }
 
-wish = 'Iphone 14 Max' #input('Что вас интересует? Напишите название товара и его конкретную характеристику: ')
+wish = 'Redmi 11' #input('Что вас интересует? Напишите название товара и его конкретную характеристику: ')
 
 info = {}
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument("--disable-extensions")
+options.add_experimental_option('useAutomationExtension', False)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-popup-blocking")
+options.add_argument("--disable-notifications")
+options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(options=options)
+# driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
 wait = WebDriverWait(driver, 10)
 
 def find_first_item_price(url:str, search_path:str , item_card_path:str, price_path:str):
-    try:
+    # try:
         driver.get(url)
         time.sleep(1)
         search = wait.until(EC.presence_of_element_located((By.XPATH, search_path)))
@@ -50,11 +63,17 @@ def find_first_item_price(url:str, search_path:str , item_card_path:str, price_p
         time.sleep(2)
         price = driver.find_element(By.XPATH, price_path).text
         return price, driver.current_url
-    except NoSuchElementException:
-        print("На сайте", url, "товар не найден", "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
-        return "Не найдено"
+    # except NoSuchElementException:
+    #     print("На сайте", url, "товар не найден")
+    #     return "Не найдено"
+
+def find_best_price():
+    sorted_price = sorted(info.items(), key=lambda x: x[1])
+    return sorted_price
+
 for name, site in websites.items():
     price = find_first_item_price(site.url, site.search_xpath, site.item_card_xpath, site.price_xpath)
     info[name] = price
     driver.quit
 print(info)
+print(find_best_price())
